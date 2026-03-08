3cc78a99f8e46c5602f4f795b8e52b025818c856
raiyannooryrady <raiyannooryrady@gmail.com>
2026-03-08 22:51:15 +0600

    update index.html title and wrap logo image in anchor tag; add test.js for DOM logging

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.URL);
+console.log(document.all);
+console.dir(document);
+// for(let element of document.all){
+//     console.log(element);
+// }
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
